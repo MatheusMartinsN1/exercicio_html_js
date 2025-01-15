aa6e65da8c624db59dd7afe748adbe6b5a6837d6
MatheusMartinsN1 <mateusdecampos34@icloud.com>
2025-01-15 20:16:28 -0300

    Exercício de intragração do JS ao HTML

---
+++ main.js
@@ -0,0 +1,11 @@
+let num1 = window.document.getElementById('num1')
+let num2 = window.document.getElementById('num2')
+let res = window.document.getElementById('res')
+
+function enviar() {
+    if(num1.value < num2.value) {
+        res.innerHTML = '<p>Muito bem! O <strong>campo B</strong> é maior que o <strong>campo A</strong> &#x1F601</p>'
+    }else {
+        res.innerHTML = '<p>Poxa, o <strong>campo A</strong> é maior que o <strong>campo B</strong> &#x1F61E</p>'
+    }
+}
