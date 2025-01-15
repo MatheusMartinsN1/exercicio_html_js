92ad491f08a46e3f8f1809e934ab958d0a36b71b
MatheusMartinsN1 <mateusdecampos34@icloud.com>
2025-01-15 20:29:05 -0300

    Exercício de integração do JS ao HTML

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ let res = window.document.getElementById('res')
 
 function enviar() {
     if(num1.value < num2.value) {
-        res.innerHTML = '<p>Muito bem! O <strong>campo B</strong> é maior que o <strong>campo A</strong> &#x1F601</p>'
+        res.innerHTML = '<p>Muito bem! O <strong>campo B</strong> é maior que o <strong>campo A</strong>. &#x1F601</p>'
     }else {
-        res.innerHTML = '<p>Poxa, o <strong>campo A</strong> é maior que o <strong>campo B</strong> &#x1F61E</p>'
+        res.innerHTML = '<p>Poxa, o <strong>campo A</strong> é maior que o <strong>campo B</strong>. &#x1F61E </p>'
+    }    if(num1.value == num2.value) {
+        window.alert('Por favor, adicione outros valores aos campos pois ambos tem o mesmo valor!')
+        res.innerHTML = ''
     }
 }
